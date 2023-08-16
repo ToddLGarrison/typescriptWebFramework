635e1b762128f77ac375921698e7f4c3eadc2e57
Todd Garrison <toddlgarrison@gmail.com>
2023-08-16 14:10:54 -0400

    Refactor sync() save method

---
+++ src/models/Sync.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse } from 'axios';
+import { UserProps } from "./User"
+
+export class Sync {
+    constructor(public rootUrl: string) {}
+    
+    fetch():void {
+        axios.get(`${this.rootUrl}/${this.get('id')}`)
+        .then((response: AxiosResponse):void => {
+            this.set(response.data);
+        });
+    }
+
+    save(data: UserProps):void {
+        const { id } = data;
+
+        if(id) {
+            axios.put(`${this.rootUrl}/${id}`, data)
+        } else {
+            axios.post(this.rootUrl, data)
+        }
+    }
+}
