5810ff5ad7168e8a6e0145dd30a7743959f96829
Todd Garrison <toddlgarrison@gmail.com>
2023-08-21 15:53:20 -0400

    Create ModelAttributes class

---
+++ src/models/Model.ts
@@ -0,0 +1,16 @@
+interface ModelAttributes{
+
+}
+
+interface Sync {
+
+}
+
+interface Events {
+    on(eventName: string, callback: () => void): void;
+    trigger(eventName: string): void;
+}
+
+export class Model {
+
+}
