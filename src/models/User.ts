b616a0727ca61bac92048e22f5f818ca76515049
Todd Garrison <toddlgarrison@gmail.com>
2023-08-21 14:21:29 -0400

    Create getter methods for User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,7 +19,15 @@ export class User {
         this.attributes = new Attributes<UserProps>(attrs)
     }
 
-    on(eventName: string, callback): void{
-        this.events.on(eventName, callback);
+    get on(){
+        return this.events.on;
+    }
+
+    get trigger(){
+        return this.events.trigger;
+    }
+
+    get get(){
+        return this.attributes.get;
     }
 }
